498a312296e10e6c89a10ce201aa31a0e2a33fc6
bennettem <emily.pamelabennett@gmail.com>
2023-09-16 18:36:53 -0500

    email check refactored

--- script.js
+++ script.js
@@ -21,10 +21,14 @@ function showSuccess(input, message) {
 }
 
 //check if email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
 }
 
 //check required fields
@@ -38,6 +42,23 @@ function checkedRequired(inputArr) {
   });
 }
 
+//check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
 //get field name
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -48,4 +69,7 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   checkedRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
 });
