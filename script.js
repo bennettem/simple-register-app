5b093638437a36aea21ac0cf7f7810a8fe94faca
bennettem <emily.pamelabennett@gmail.com>
2023-09-16 14:12:53 -0500

    error messages display on all inputs

--- script.js
+++ script.js
@@ -29,4 +29,22 @@ form.addEventListener("submit", function (e) {
   } else {
     showSuccess(username);
   }
+
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value === "") {
+    showError(password2, "Password2 is required");
+  } else {
+    showSuccess(password2);
+  }
 });
