db3d3815d3fde1d67a854242b8c17a236b1ee617
bennettem <emily.pamelabennett@gmail.com>
2023-09-16 14:10:26 -0500

    show error inputs/success input messages

--- script.js
+++ script.js
@@ -6,18 +6,27 @@ const password2 = document.getElementById("password2");
 
 //show input error message
 function showError(input, message) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control error';
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
 }
 
+//show input success message
+function showSuccess(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
 
 //event listeners
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    if(username.value === '') {
-        showError(username, 'Username is required');
-    } else {
-        showSuccess(username);
-    }
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
 });
